825c289779f88d705ce5a8589810b0c962996b3d
Александр Мокрушин <neonamenemoone@yandex.ru>
2023-10-19 10:23:12 +0300

    central

--- shared_desktop/tools/forms.py
+++ shared_desktop/tools/forms.py
@@ -4,5 +4,10 @@ from .models import Central
 class ControllerForm(forms.ModelForm):
 
     class Meta:
+
         model = Central
         fields = ['ip_address', 'phase_value']
+        widgets = {
+            'ip_address': forms.TextInput(attrs={'class': 'form-control'}),
+            'phase_value': forms.Select(attrs={'class': 'form-control'}),
+        }
